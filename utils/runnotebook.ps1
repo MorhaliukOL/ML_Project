e24b7d957c8971ee7cd92b47e3eb7011c80005dd
Kurenkov27 <36358857+Kurenkov27@users.noreply.github.com>
2021-05-30 15:49:00 +0300

    Update runnotebook.ps1

--- utils/runnotebook.ps1
+++ utils/runnotebook.ps1
@@ -3,4 +3,4 @@
 # see script builddocker.ps1 for how to build the image
 # it also maps the project root directory to a folder within the docker image
 
-docker run -v ${PWD}:/tf -it --rm -p 8888:8888 ml_project:v1
+sudo docker run -v ${PWD}:/tf -it --rm -p 8888:8888 ml_project:v1
