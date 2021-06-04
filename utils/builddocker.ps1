71903f53a830b6076f83bf4baf4f639c2dda8f8b
unknown <morhaliuk@gmail.com>
2021-06-04 15:39:17 +0300

    remove 'sudo' from scripts

--- utils/builddocker.ps1
+++ utils/builddocker.ps1
@@ -1,13 +1,12 @@
 #!/bin/bash
 # Builds a docker image for ML_Project
-# takes 1 argument which is the name of a dockerfile e.g. train-gpu.Dockerfile
+# takes 1 argument - path to a dockerfile e.g. './install/train-gpu.Dockerfile'
 # It assumes all Dockerfiles are in ML_Project/install/ directory
 
-$Dockerfile = $args[0]
 
-# $INSTALL_DIR must contain Dockerfiles and all files that
+# $INSTALL_DIR - directory that contains Dockerfiles and all files that
 # need to be copied to docker image, like 'requirements.txt'
 $INSTALL_DIR=Join-Path ${PWD} "install"
-$DOCKERFILE_PATH=Join-Path $INSTALL_DIR $Dockerfile
+$DOCKERFILE_PATH=$args[0]
 
-sudo docker build -t "ml_project:v1" -f $DOCKERFILE_PATH $INSTALL_DIR
+docker build -t "ml_project:v1" -f $DOCKERFILE_PATH $INSTALL_DIR
